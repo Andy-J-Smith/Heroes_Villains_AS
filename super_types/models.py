f48233b40a1c412d01d785f824f2033d47abc4c0
Andy-J-Smith <98605897+Andy-J-Smith@users.noreply.github.com>
2022-02-18 12:05:33 -0600

    Updated models.py + Updated serializers.py for supers app + migrated changes to db

--- super_types/models.py
+++ super_types/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 class SuperTypes(models.Model):
-    type = models.CharField(max_length=255)
+    type = models.CharField(max_length=255,null=True)
 
     def __str__(self) -> str:
         return self.type
