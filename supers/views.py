da9277fcf9ed5f90b1eed491aba6c2a26951d580
Andy-J-Smith <98605897+Andy-J-Smith@users.noreply.github.com>
2022-02-20 20:21:56 -0600

    Added request.query params to GET function.

--- supers/views.py
+++ supers/views.py
@@ -6,35 +6,36 @@ import super_types
 from .serializers import SuperSerializer
 from .models import Super
 from rest_framework.response import Response
+from django_filters import filters
+        # filterset_fields = ['id','name', 'alter_ego', 'primary_ability', 'secondary_ability', 'catchphrase', 'super_type', 'super_type_id']
 
 
 # Create your views here.
 @api_view(['GET', 'POST'])
-
 def supers_list(request):
+    type_param = request.query_params.get('super_type')
     supers = Super.objects.all()
-    
-    if type in request.GET and request.GET['hero']:
-        hero = request.GET['hero']
-        types = supers.filter(super_types__icontains='hero')
-    elif type in request.GET and request.GET['hero']:
-        villain = request.GET['villain']
-        types = supers.filter(super_types__icontains='villain')
-        context = {'super_type':super_types}
-        return Response(request, context)
-    elif request.method == 'GET':
+    if type_param:
+        supers = supers.filter(super_type__type=type_param)
         serializer = SuperSerializer(supers, many=True)
         return Response(serializer.data)
-    elif request.method == 'GET':
-        serializer = supers.filter(super_type__lookup_name='villain')
+    if request.method == 'GET':
+        serializer = SuperSerializer(supers, many=True)
         return Response(serializer.data)
+        
     elif request.method == 'POST':
         serializer = SuperSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         serializer.save()
         return Response(serializer.data, status=status.HTTP_201_CREATED)
-    
 
+# @api_view(['GET', 'PUT'])
+# def query_set(request):
+#     query_set = Super.objects.all()
+#     hero = request('hero')
+#     if hero is not None:
+#         query_set = query_set.filter(super_types=hero)
+#     return query_set
     
 
 @api_view(['GET', 'PUT', 'DELETE'])
